5692887cff0df3ded0ddc18fda1e3b96a573b43c
Colin LeMesurier <43153081+clemesu1@users.noreply.github.com>
2022-05-12 12:16:38 -0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,12 +1,10 @@
 import React from "react";
-import Counter from "./components/Counter";
 import TallyCounter from "./components/TallyCounter";
 import "./styles.css";
 
 export default function App() {
   return (
     <div className="App">
-      {/* <Counter /> */}
       <TallyCounter />
     </div>
   );
